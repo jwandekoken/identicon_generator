fb65cf50f15ad5e6422c888d4e2bc140af865d8c
jwandekoken <julio.wandekoken@paketa.com.br>
2021-08-13 06:37:43 -0300

    updated image struct with r,g,b colors based on the md5 hash of the inputed string

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,12 +1,20 @@
 defmodule Identicon do
+  require Logger
+
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(%{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list()
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
